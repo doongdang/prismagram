eaee4a62f0a24464b3b29af4492628f5cde75223
SeokHyun Cho <doongdang@naver.com>
2020-03-25 18:39:14 +0900

    #1.2
    Make Schemas & Reslovers

--- src/server.js
+++ src/server.js
@@ -1,24 +1,22 @@
 require("dotenv").config();
-import { GraphQLServer } from "graphql-yoga";
+import {
+  GraphQLServer
+} from "graphql-yoga";
+import logger from "morgan"
+import schema from "./schema"
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
 
-`;
-const resolvers = {
-  Query: {
-    hello: () => "Hi"
-  }
-};
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  schema
+});
 
 server.express.use(logger("dev"));
 
-server.start({ port: PORT }, () =>
-  console.log(`Server Runnig on https://localhost:${PORT}`)
+server.start({
+    port: PORT
+  }, () =>
+  console.log(`✅Server Runnig on https://localhost:${PORT}`)
 );
