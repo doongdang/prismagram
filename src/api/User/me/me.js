c3987b3b28fdd2f4cd2f47c4b507e078077d9473
SeokHyun Cho <doongdang@naver.com>
2020-04-08 16:12:43 +0900

    Update me.js

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,32 +1,14 @@
-import {
-    prisma
-} from "../../../../generated/prisma-client"
-import {
-    USER_FRAGMENT
-} from "../../../fragment"
+import { prisma } from "../../../../generated/prisma-client";
+import { USER_FRAGMENT } from "../../../fragment";
 
 export default {
-    Query: {
-        me: async (_, __, {
-            request,
-            isAuthenticated
-        }) => {
-            isAuthenticated(request)
-            const {
-                user
-            } = request
-            const useProfile = await prisma.user({
-                id: user.id
-            })
-            const posts = await prisma.user({
-                id: user.id
-            }).posts()
-            return {
-                user: useProfile,
-                posts
-            }
-
-        }
-    }
-
-}
+  Query: {
+    me: async (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      return await prisma.user({
+        id: user.id,
+      });
+    },
+  },
+};
